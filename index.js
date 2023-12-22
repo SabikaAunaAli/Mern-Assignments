1271a344cb439bb45a68bd709f298bcc334dc872
samana <mazinmahmood25@gmail.com>
2023-12-22 10:18:10 +0500

    Team work updated

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-// // console.log("This is my Package-JSON file");
-// console.log(process.argv);
-// function grab(name){
-//     var index= process.argv.indexOf(name)
-//     return(index === -1) ? null: process.argv[index]
-// }
-// console.log(index);
-
-console.log("Please Enter You Name");
-process.stdin.once('data' , data =>{
-
-console.log("Welcome " + data  + "to our website");
-// process.exit();
-
-
-})
 
 console.log("git branches");
 
-console.log("Sabika's git Work");
+console.log("team git Work");
