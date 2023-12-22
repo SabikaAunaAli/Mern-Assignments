a4bc99afb1424cdddbd319c5288a1ffe002d2b4b
samana <mazinmahmood25@gmail.com>
2023-12-22 10:16:00 +0500

    sabika work change

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-// // console.log("This is my Package-JSON file");
-// console.log(process.argv);
-// function grab(name){
-//     var index= process.argv.indexOf(name)
-//     return(index === -1) ? null: process.argv[index]
-// }
-// console.log(index);
 
-console.log("Please Enter You Name");
-process.stdin.once('data' , data =>{
-
-console.log("Welcome " + data  + "to our website");
-// process.exit();
-
-
-})
+console.log("Here is sabika's work");
